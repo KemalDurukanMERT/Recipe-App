80ec1496db11c2a419bd41dd342d3c9b4497fd4e
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-11-05 17:50:12 +0100

    Login Form

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import {useNavigate} from "react-router-dom"
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventdefault();
+    navigate(-1)
+  }
+
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>{"<CW/>"}Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="Enter username" type="text"/>
+          <StyledInput placeholder="Enter password" type="password"/>
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
