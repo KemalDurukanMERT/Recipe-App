8a93fb1cd7b9a57c30a1a63a300a299ca5667404
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-11-08 18:45:59 +0100

    Card Component

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,8 @@
-import {Link} from "react-router-dom"
+// import {Link} from "react-router-dom"
 import React from "react";
 import {Nav} from "./Navbar.style";
-import {Brand} from "./Navbar.style";
+import {Brand, Menu, MenuLink} from "./Navbar.style";
+
 
 const Navbar = () => {
   return (
@@ -10,12 +11,12 @@ const Navbar = () => {
         <i>{"<Clarusway/>"}</i>
         <span>Recipe</span>
       </Brand>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="about">About</Link>
-        <Link to="register">Register</Link>
-        <Link to="logout">Logout</Link>
-      </div>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="about">About</MenuLink>
+        <MenuLink to="register">Register</MenuLink>
+        <MenuLink to="logout">Logout</MenuLink>
+      </Menu>
     </Nav>
   );
 };
