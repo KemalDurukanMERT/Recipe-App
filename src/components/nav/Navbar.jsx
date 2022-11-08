59a250cd416cdb5758dcb72f5beee7560a5c6781
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-11-08 18:58:05 +0100

    Navbar

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,21 +1,26 @@
-// import {Link} from "react-router-dom"
-import React from "react";
-import {Nav} from "./Navbar.style";
-import {Brand, Menu, MenuLink} from "./Navbar.style";
-
+import React,{useState} from "react";
+import "./Navbar.style";
+import Nav, { Brand ,Menu,MenuLink,Hamburger} from "./Navbar.style";
+import {GiHamburgerMenu} from "react-icons/gi"
 
 const Navbar = () => {
+  const [isOpen,setIsOpen]=useState(false)
   return (
     <Nav justify="space-between" wrap="wrap">
       <Brand to="/">
         <i>{"<Clarusway/>"}</i>
         <span>Recipe</span>
       </Brand>
-      <Menu>
+
+      <Hamburger onClick={()=>setIsOpen(!isOpen)}>
+        <GiHamburgerMenu/>
+      </Hamburger>
+
+      <Menu isOpen={isOpen} onClick={()=>setIsOpen(false)}>
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="about">About</MenuLink>
         <MenuLink to="register">Register</MenuLink>
-        <MenuLink to="logout">Logout</MenuLink>
+        <MenuLink to="Logout" onClick={()=>sessionStorage.clear()}>Logout</MenuLink>
       </Menu>
     </Nav>
   );
--- src/components/nav/Navbar.style.jsx
+++ src/components/nav/Navbar.style.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
-import Flex from "../GlobalStyles/Flex.style";
+import Flex from "../globalStyles/Flex.style";
 import { Link } from "react-router-dom";
 
-export const Nav = styled(Flex)`
+const Nav = styled(Flex)`
   padding: 1rem 1.5rem;
   background-color: ${({ theme }) => theme.colors.navbarBgColor};
 `;
@@ -20,19 +20,11 @@ export const Brand = styled(Link)`
 `;
 
 export const Menu = styled(Flex)`
-  text-align: center;
-  padding: 1rem 2rem;
-  cursor: pointer;
-  text-decoration: none;
-  color: ${({ theme }) => theme.colors.linkColor};
-  transition: all 0.3s ease-in;
-  &:hover {
-    color: ${({ theme }) => theme.colors.mainColor};
-    font-weight: bold;
-  }
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
+    flex-direction:column;
     width: 100%;
-    border-bottom: 2px solid ${({ theme }) => theme.colors.mainColor};
+    max-height: ${({isOpen})=>(isOpen ? "400px" : "0")};
+    overflow: hidden;
   }
 `;
 
@@ -53,13 +45,13 @@ export const MenuLink = styled(Link)`
   }
 `;
 
-
 export const Hamburger = styled.div`
   display: none;
   cursor: pointer;
   color: ${({ theme }) => theme.colors.mainColor};
-
   @media (max-width: ${({ theme }) => theme.screens.lg}) {
     display: block;
   }
 `;
+
+export default Nav;
