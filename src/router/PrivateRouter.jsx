93256b0ad2fe27742181ee7b98376b2afe04d71f
KemalDurukanMERT <kdurukanmert@gmail.com>
2022-12-17 17:57:42 +0100

    .

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -2,10 +2,9 @@ import React from 'react'
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PrivateRouter = () => {
-  const user = true;
   return (
     <div>
-      {user ? <Outlet/> : <Navigate to="/login"/>}
+      {sessionStorage.getItem("user") ? <Outlet/> : <Navigate to="/login"/>}
     </div>
   )
 }
